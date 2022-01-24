604dde1eee67d061b4771be24a04d2b131c4e3c0
Venti <70521040+s11029568@users.noreply.github.com>
2022-01-24 09:22:39 -0500

    v0.2

--- JacksonStanton00.java
+++ JacksonStanton00.java
@@ -1,14 +1,13 @@
-// Maxx Secrest, Arrays Practice, v0.0, 01/24/22
+// Maxx Secrest, Arrays Practice, v0.2, 01/24/22
 
 public class JacksonStanton00 {
 
     public static void main(String[] args) {
 
-        String[] carArr  = {"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lightning", "Buick Skylark"};
-        int[] myGrades  = { 100, 25, 15, -3, 75, 35};
-        double myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+        
+        
 
-        for(current_element = 0; current_element < carArr.length(); current _element += 1) {
+      /*  for(current_element = 0; current_element < carArr.length(); current _element += 1) {
             // code to execute
             // Statement 1 is executed ONCE BEFORE THE LOOP.
             // Statement 2 is the CONDITIONAL for the loop.
@@ -16,9 +15,30 @@ public class JacksonStanton00 {
 
         }
 
+        
         for (i = 0; i <carArr.length(); i++) {
 
-            
+
+        }
+        */
+
+        String[] carArr  = {"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lightning", "Buick Skylark"};
+        for (String eachElement : carArr ) {
+            // Code to execute.
+            System.out.println(eachElement);
+
+        }
+        int[] myGrades  = { 100, 25, 15, -3, 75, 35};
+        for (int eachElement : myGrades ) {
+            // Code to execute.
+            System.out.println(eachElement);
+
+        }
+        double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+        for (double eachElement : myBankAccount ) {
+            // Code to execute.
+            System.out.println(eachElement);
+
         }
 
 
