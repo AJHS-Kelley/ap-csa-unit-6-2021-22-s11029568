099f6558208c6c191e8be18407752461c0e61640
Venti <70521040+s11029568@users.noreply.github.com>
2022-01-24 09:06:43 -0500

    v0.1

--- JacksonStanton00.java
+++ JacksonStanton00.java
@@ -8,7 +8,19 @@ public class JacksonStanton00 {
         int[] myGrades  = { 100, 25, 15, -3, 75, 35};
         double myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
 
-        
+        for(current_element = 0; current_element < carArr.length(); current _element += 1) {
+            // code to execute
+            // Statement 1 is executed ONCE BEFORE THE LOOP.
+            // Statement 2 is the CONDITIONAL for the loop.
+            // Statement 3 is executed EACH TIME THE LOOP RUNS.
+
+        }
+
+        for (i = 0; i <carArr.length(); i++) {
+
+            
+        }
+
 
     }
 
