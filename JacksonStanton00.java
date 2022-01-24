a941f9c9f22ed011dac5038c443c6953173f2c54
Venti <70521040+s11029568@users.noreply.github.com>
2022-01-24 08:48:15 -0500

    v0.0

---
+++ JacksonStanton00.java
@@ -0,0 +1,24 @@
+// Maxx Secrest, Arrays Practice, v0.0, 01/24/22
+
+public class JacksonStanton00 {
+
+    public static void main(String[] args) {
+
+        String[] carArr  = {"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lightning", "Buick Skylark"};
+        int[] myGrades  = { 100, 25, 15, -3, 75, 35};
+        double myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
